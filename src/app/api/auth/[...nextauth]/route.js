6996c1d6cf0a35b86572b1209f16ff0b52b15e87
Fazila Bano <63119868+F172000@users.noreply.github.com>
2025-02-21 10:59:24 +0500

    updated the route

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import fs from "fs";
-import path from "path";
 
 export const authOptions = {
   providers: [
@@ -12,25 +10,14 @@ export const authOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        const res = await fetch(`${process.env.NEXTAUTH_URL}/auth.json`);
-        if (!res.ok) {
-          throw new Error("Failed to fetch user data");
-        }
-
-        const users = await res.json();
-
-        const user = users.find(
-          (u) =>
-            u.email === credentials.email && u.password === credentials.password
-        );
-
-        if (user) {
-          return { email: user.email };
+        if (credentials.email === "test@example.com" && credentials.password === "password") {
+          return { email: credentials.email };
         }
         return null;
       },
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET, 
   pages: {
     signIn: "/auth",
   },
