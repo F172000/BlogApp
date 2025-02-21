d2609cce1424b77f95de5aa6d0f054506ee992ca
Fazila Bano <63119868+F172000@users.noreply.github.com>
2025-02-21 10:26:01 +0500

    added console log message

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -18,6 +18,7 @@ export default function SignIn() {
       redirect: false,
     });
     if (result?.ok) {
+      console.log(result,"inside ok>>>>>>>>>>>");
       setLoading(false);
       router.push("/blog");
     } else {
